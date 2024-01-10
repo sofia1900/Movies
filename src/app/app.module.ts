39c8971c1f5b56f0a8a3c16c1d4f620fe57c8d42
sofia <sofia.jimlap@educa.jcyl.es>
2024-01-10 17:35:56 +0100

    service1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MovieListComponent } from './movie-list/movie-list.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MovieListComponent
+    MovieListComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
--- src/app/movie-list/movie-list.component.ts
+++ src/app/movie-list/movie-list.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MovieService} from "../service/movie.service";
+import {Movie} from "../Shared/Movie";
 
 @Component({
   selector: 'app-movie-list',
   templateUrl: './movie-list.component.html',
   styleUrl: './movie-list.component.css'
 })
-export class MovieListComponent {
+export class MovieListComponent implements OnInit{
 
+  movies : Movie[] = [];
+  constructor(private movieService : MovieService) {}
+
+  ngOnInit(){
+    this.movieService.getAllMovies()
+      .subscribe( m =>
+        this.movies = m
+      )
+  }
+
+  alerta (movie : Movie) {
+    alert(`Movie: ${movie.Title}`)
+  }
 }
